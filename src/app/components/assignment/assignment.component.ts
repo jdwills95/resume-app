c12941f2c7b9c0cf3c921ce7acda90f80d7c0a23
jdwills95 <jdwills95@gmail.com>
2021-06-02 16:28:15 -0500

    Formatted HTML

--- src/app/components/assignment/assignment.component.ts
+++ src/app/components/assignment/assignment.component.ts
@@ -24,20 +24,20 @@ export class AssignmentComponent implements OnInit {
         startDate: assignment.startDate,
         desc: assignment.desc,
         environments: this.getEnvironmentsString(assignment.environments),
-      }
+      };
     });
-    
+
   }
 
   getEnvironmentsString(environments: string[]): string {
-    let environmentsString: string = '';
+    let environmentsString = '';
     environments.forEach((environment: string, index) => {
       if (index !== 0) {
         environmentsString = environmentsString + ', ' + environment;
       } else {
         environmentsString = environment;
       }
-    })
+    });
     return environmentsString;
   }
 
