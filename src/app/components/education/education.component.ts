c334200f3ab9763f3dbb079db22f028c691d6e40
Joey Wills <jdwills95@gmail.com>
2021-06-11 20:19:38 -0500

    Added content and styling for education componet

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { IEducation } from 'src/app/interfaces/education';
+
+import educationJSON from 'src/app/data/education.json';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  education: IEducation[] = this.getEducation(educationJSON);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getEducation(educationJSON: IEducation[]): IEducation[] {
+    return educationJSON.map((degree) => {
+      return {
+        degree: degree.degree,
+        field: degree.field,
+        school: {
+          name: degree.school.name,
+          location: degree.school.location,
+        },
+        graduationYear: degree.graduationYear,
+      };
+    });
   }
-
 }
