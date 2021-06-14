f66737d35ed005b252a8e325eb5f40a5a67ede3c
Joey Wills <jdwills95@gmail.com>
2021-06-13 20:46:51 -0500

    Added resume download pdf button

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  downloadResume() {
+    let link = document.createElement('a');
+    link.download = 'Resume_Joey_Wills';
+    link.href = 'assets/resume/Resume_Wills_Joey.pdf';
+    link.click();
   }
-
 }
