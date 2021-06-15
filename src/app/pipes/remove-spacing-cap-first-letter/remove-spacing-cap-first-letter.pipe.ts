ad2f6fedb5d72ec54fa6f47051ff38d7d2cbb04b
jdwills95 <jdwills95@gmail.com>
2021-06-15 14:18:51 -0500

    Added RemoveSpacingCapFirstLetterPipe

---
+++ src/app/pipes/remove-spacing-cap-first-letter/remove-spacing-cap-first-letter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'removeSpacingCapFirstLetter',
+})
+export class RemoveSpacingCapFirstLetterPipe implements PipeTransform {
+  transform(value: string): string {
+    let splitStr = value.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join('');
+  }
+}
