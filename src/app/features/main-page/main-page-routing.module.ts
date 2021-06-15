90645fec095304692fc23aca72a7ba30c6528bb5
jdwills95 <jdwills95@gmail.com>
2021-06-15 18:08:34 -0500

    Messed with some more styling

--- src/app/features/main-page/main-page-routing.module.ts
+++ src/app/features/main-page/main-page-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from 'src/app/features/main-page/main-page.component';
 
 const routes: Routes = [{ path: '', component: MainPageComponent }];
-//const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
