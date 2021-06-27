6801786c8d7cc4440dd2d31a967b4a3587a116c0
Joey Wills <jdwills95@gmail.com>
2021-06-26 22:35:35 -0500

    Fixed linting errors

--- src/app/services/scroll/scroll.service.ts
+++ src/app/services/scroll/scroll.service.ts
@@ -2,30 +2,15 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ScrollService {
-  private styleTag: HTMLStyleElement;
-
-  constructor() {
-    this.styleTag = this.buildStyleElement();
-  }
-
   public disable(): void {
-    document.body.appendChild(this.styleTag);
+    const x = window.scrollX;
+    const y = window.scrollY;
+    window.onscroll = () => {
+      window.scrollTo(x, y);
+    };
   }
+
   public enable(): void {
-    document.body.removeChild(this.styleTag);
-  }
-
-  private buildStyleElement(): HTMLStyleElement {
-    const style = document.createElement('style');
-
-    style.type = 'text/css';
-    style.setAttribute('data-debug', 'Injected by WindowScrolling service.');
-    style.textContent = `
-                body {
-                    overflow: hidden !important ;
-                }
-            `;
-
-    return style;
+    window.onscroll = () => {};
   }
 }
