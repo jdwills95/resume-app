e61d4dc58c386a8cc2d9b810b867331ccf1d99f4
jdwills95 <jdwills95@gmail.com>
2021-08-11 16:14:14 -0500

    Added unit test for CurrentScreenWidthService service

---
+++ src/app/services/current-screen-width/current-screen-width.service.spec.ts
@@ -0,0 +1,85 @@
+import { CurrentScreenWidthService } from 'src/app/services/current-screen-width/current-screen-width.service';
+
+describe('RemoveSpacingCapFirstLetterPipe', () => {
+  const service = new CurrentScreenWidthService();
+
+  it('should return true when set to window innerWidth is 576', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 576,
+    });
+
+    expect(service.isScreenWidthPhoneOrBigger()).toBe(true);
+  });
+
+  it('should return false when set to window innerWidth is 575', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 575,
+    });
+
+    expect(service.isScreenWidthPhoneOrBigger()).toBe(false);
+  });
+
+  it('should return true when set to window innerWidth is 768', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 768,
+    });
+
+    expect(service.isScreenWidthTabletOrBigger()).toBe(true);
+  });
+
+  it('should return false when set to window innerWidth is 767', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 767,
+    });
+
+    expect(service.isScreenWidthTabletOrBigger()).toBe(false);
+  });
+
+  it('should return true when set to window innerWidth is 992', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 992,
+    });
+
+    expect(service.isScreenWidthDesktopOrBigger()).toBe(true);
+  });
+
+  it('should return false when set to window innerWidth is 991', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 991,
+    });
+
+    expect(service.isScreenWidthDesktopOrBigger()).toBe(false);
+  });
+
+  it('should return true when set to window innerWidth is 1200', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 1200,
+    });
+
+    expect(service.isScreenWidthLargeDesktopOrBigger()).toBe(true);
+  });
+
+  it('should return false when set to window innerWidth is 1199', () => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 1199,
+    });
+
+    expect(service.isScreenWidthLargeDesktopOrBigger()).toBe(false);
+  });
+});
