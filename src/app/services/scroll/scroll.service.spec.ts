51ce77dd1bed08a0997fa2ee28c6d7892532e9ad
jdwills95 <jdwills95@gmail.com>
2021-08-23 12:15:42 -0500

    Added Unit test to nav-bar service.

---
+++ src/app/services/scroll/scroll.service.spec.ts
@@ -0,0 +1,9 @@
+import { ScrollService } from 'src/app/services/scroll/scroll.service';
+
+describe('ScrollService', () => {
+  const service = new ScrollService();
+
+  it('should create', () => {
+    expect(service).toBeTruthy();
+  });
+});
